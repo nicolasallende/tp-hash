d37fb5c7782313fd0a3455a3f1b48e24aa569009
Nicolas Allende <allenden96@gmail.com>
2017-05-03 18:11:05 -0300

    guardar y obtener

--- hash.c
+++ hash.c
@@ -1,6 +1,8 @@
-#include "hash.h"
+//#include "hash.h"
 #include <stdlib.h>
 #include <stdbool.h>
+
+int const MAX = 70;
 
 typedef enum {BORRADO,VACIO,OCUPADO} estado_t;
 
@@ -45,5 +47,48 @@ hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
 }
 
 
-
-
+bool hash_guardar(hash_t *hash, const char *clave, void *dato){
+	if(hash_pertenece(hash,clave)){
+		int i = 0;
+		while(hash->tabla[i]->clave != clave){
+			i++;
+		}
+		if(hash->destructor != NULL){
+			hash->destructor(hash->tabla[i]->dato);	 //esto se va a repetir hay que hacer una funcion  sobreescribir que se use con los borrados y otros
+		}
+		hash->tabla[i]->dato = dato;
+		return true;
+	}
+	if(((hash->cantidad / hash->tamanio)*100) >= MAX){
+		//aca es lo de redimensionar 
+	}
+	size_t h = func_hash(clave);
+	while(h > hash->tamanio){
+		h = h / (hash->tamanio);
+	}
+	while((hash->tabla[h]->estado != VACIO) && (hash->tabla[h]->estado != BORRADO)){
+		h++;
+		if(h = hash->tamanio){
+			h = 0;
+		}
+	}
+	if(hash->tabla[h]->estado == BORRADO){
+		if(hash->destructor != NULL){			
+			hash->destructor(hash->tabla[h]->dato);	 //esto se va a repetir hay que hacer una funcion  sobreescribir que se use con los borrados y otros
+		}
+	}
+	hash->tabla[h]->clave = clave;
+	hash->tabla[h]->dato = dato;
+	hash->cantidad++;
+	hash->tabla[h]->estado = OCUPADO;
+	return true;
+}
+			
+void *hash_obtener(const hash_t *hash, const char *clave){
+	if(!hash_pertenece(hash,clave)) return NULL;
+	int i = 0;
+	while(hash->tabla[i]->clave != clave){
+		i++;
+	}
+	return hash->tabla[i]->dato;
+}
